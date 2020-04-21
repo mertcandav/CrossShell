b5e3c15e8e45bca03929151954f6bb32598e6b07
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-04-21 18:09:00 +0300

    Add clear command.

--- src/core/cmdProcessor.py
+++ src/core/cmdProcessor.py
@@ -1,4 +1,6 @@
 # Imports.
+import platform
+import os
 from core.terminal.terminal import *
 from core.functions.cd import *
 
@@ -22,6 +24,15 @@ class cmdProcessor:
             pass
         if cmd == "exit":
             exit(0)
+            pass
+        if cmd == "clear":
+            if platform.system() == "Windows":
+                os.system("cls")
+                pass
+            else:
+                os.system("clear")
+                pass
+            return
             pass
         if cmd.startswith("cd "):
             cmd = cmdProcessor.readyCmd("cd ",cmd)
