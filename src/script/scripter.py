cb4ba28082a81dd412a0ebe720a35cc26ee87fc6
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-05 20:01:24 +0300

    Add processRange to scripting.

--- src/script/scripter.py
+++ src/script/scripter.py
@@ -13,46 +13,10 @@ from script.functions.input import *
 
 class scripter:
     @staticmethod
-    def execScriptCommand(term: terminal, paths: list, cmd: str) -> bool:
-        cmd = cmd.strip()
-        lcmd = cmd.lower()
-        if lcmd == "break":
-            return False
-            pass
-        if lcmd == "cdbasepath":
-            term.CurrentPath = paths[0]
-            term.Shell.Update()
-            return True
-            pass
-        if lcmd == "cdscriptpath":
-            term.CurrentPath = paths[1]
-            term.Shell.Update()
-            return True
-            pass
-        
-        werr(scriptRuntimeErrors, f"'{ORANGE}{cmd}{RED}' Script command not recognized!", 0)
-        return
-        pass
-
-    @staticmethod
-    def interpret(term: terminal, path: str) -> None:
-        # Read code.
-        code = fs.readAllText(path, "utf-8")
-        
-        # Prepare the code.
-        code = codeProcessor.clearComments(code)
-
-        basePath = term.CurrentPath
-
-        term.CurrentPath = func_cd.process(path, "..")
-        term.Shell.Update()
-
-        variables = []
-        commands = codeProcessor.getCommands(code)
-
+    def processRange(term: terminal, paths: list, variables: list, commands: list):
         for command in commands:
             if command.startswith(">"):
-                if scripter.execScriptCommand(term, [ basePath, func_cd.process(path, "..") ], command[1:]) == False:
+                if scripter.execScriptCommand(term, paths, command[1:]) == False:
                     return
                     pass
                 continue
@@ -106,6 +70,47 @@ class scripter:
 
             pass
 
+        pass
+
+    @staticmethod
+    def execScriptCommand(term: terminal, paths: list, cmd: str) -> bool:
+        cmd = cmd.strip()
+        lcmd = cmd.lower()
+        if lcmd == "break":
+            return False
+            pass
+        if lcmd == "cdbasepath":
+            term.CurrentPath = paths[0]
+            term.Shell.Update()
+            return True
+            pass
+        if lcmd == "cdscriptpath":
+            term.CurrentPath = paths[1]
+            term.Shell.Update()
+            return True
+            pass
+        
+        werr(scriptRuntimeErrors, f"'{ORANGE}{cmd}{RED}' Script command not recognized!", 0)
+        return
+        pass
+
+    @staticmethod
+    def interpret(term: terminal, path: str) -> None:
+        # Read code.
+        code = fs.readAllText(path, "utf-8")
+        
+        # Prepare the code.
+        code = codeProcessor.clearComments(code)
+
+        basePath = term.CurrentPath
+
+        term.CurrentPath = func_cd.process(path, "..")
+        term.Shell.Update()
+
+        variables = []
+        commands = codeProcessor.getCommands(code)
+
+        scripter.processRange(term, [ basePath, func_cd.process(path, "..") ], variables, commands)
         pass
 
     pass
