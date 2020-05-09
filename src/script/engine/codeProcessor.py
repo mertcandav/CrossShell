306a4b83715bcfe8f2ead6f7991850642d2fac02
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-09 13:07:12 +0300

    Fix command processor engine.

--- src/script/engine/codeProcessor.py
+++ src/script/engine/codeProcessor.py
@@ -18,8 +18,48 @@ class codeProcessor:
     @staticmethod
     def getCommands(code: str) -> list:
         commands = []
-        for command in re.split("(?![^{]*\});", code, flags = re.UNICODE | re.MULTILINE):
-            command = command.strip()
+
+        def readFunc(commands) -> list:
+            fcommand = ""
+            openBracketCount = 0
+            counter = 0
+            for command in commands:
+                counter += 1
+                dex = command.find("{")
+                if dex != -1:
+                    openBracketCount += 1
+                    pass
+                fdex = command[dex:].find("}")
+                if fdex != -1:
+                    if openBracketCount > 0:                   
+                        openBracketCount -= 1
+
+                        if openBracketCount == 0:
+                            fcommand += command + ";"
+                            break
+                            pass
+                        pass
+                    
+                    pass
+
+                fcommand += command + ";"
+                pass
+
+            return [ fcommand[:fcommand.__len__()-1].strip(), commands[counter-1:] ]
+            pass
+
+        parts = code.split(";")
+        dex = -1
+        while(dex < parts.__len__()-1):
+            dex += 1
+            command = parts[dex].strip()
+
+            if command.find("{") != -1:
+                result = readFunc(parts[dex:])
+                command = result[0]
+                parts = result[1]
+                dex = 0
+                pass
 
             if (command == "") | (command == "@") | (command == ">"):
                 continue
